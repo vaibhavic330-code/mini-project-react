bf1894b8799a978b2266a6cf496797ac7ed8e1f2
vaibhavic330-code <vaibhavic330@gmail.com>
2025-11-28 00:11:32 +0530

    implement InfoBox component to display weather data using MUI

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -1,47 +1,49 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import "./InfoBox.css"
-export default function InfoBox() {
+import CardMedia from '@mui/material/CardMedia';
+import "./InfoBox.css";
 
-  const INIT_URL = "https://plus.unsplash.com/premium_photo-1711305682236-de8d0ac5ff38?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+export default function InfoBox({ info }) {
 
-  let info = {
-    city:"delhi",
-    feeslike: 24.84,
-    temp: 25.05,
-    tempMin: 25.05,
-    tempMax: 25.05,
-    humidity: 47,
-    weather: "haze"
-  };
+  if (!info) {
+    return <h3>No data available</h3>;
+  }
 
+  const INIT_URL =
+    "https://plus.unsplash.com/premium_photo-1711305682236-de8d0ac5ff38?q=80&w=1171&auto=format&fit=crop";
+
+    const HOT_URL ="";
+    const COLD_URL ="";
+    const RAIN_URL ="";
   return (
     <div className="InfoBox">
-      <h1>Weather Info - {info.weather}</h1>
       <div className="cardContainer">
 
-      
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image={INIT_URL}   // <-- FIXED
-          title="Weather Image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Temperature: {info.temp}°C
-          </Typography>
-           {info.city}
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Feels like: {info.feeslike}°C <br />
-            Min: {info.tempMin}°C | Max: {info.tempMax}°C <br />
-            Humidity: {info.humidity}%
-          </Typography>
-        </CardContent>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={info.humidity > 80? RAIN_URL :(info.temp > 15)?HOT_URL:COLD_URL}
+            title="Weather Image"
+          />
 
-      </Card>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Temperature: {info.temp}°C
+            </Typography>
+
+            <Typography variant="h6">
+              City: {info.city}
+            </Typography>
+
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              Feels like: {info.feelsLike}°C <br />
+              Min: {info.tempMin}°C | Max: {info.tempMax}°C <br />
+              Humidity: {info.humidity}%
+            </Typography>
+          </CardContent>
+        </Card>
+
       </div>
     </div>
   );
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,7 +1,27 @@
-export default function WeatherApp (){
-    return (
-        <div>
-            
-        </div>
-    )
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+import { useState } from "react";
+
+export default function WeatherApp() {
+ const [weatherInfo, setWeatherInfo] = useState({
+  city: "delhi",
+  feelsLike: 24.84,
+  temp: 25.05,
+  tempMin: 25.05,
+  tempMax: 25.05,
+  humidity: 47,
+  weather: "haze"
+});
+
+  let updatedInfo = (newinfo) => {
+    setWeatherInfo(newinfo);
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Weather App by Delta</h2>
+      <SearchBox updatedInfo={updatedInfo} />
+      <InfoBox info={weatherInfo} />
+    </div>
+  );
 }
